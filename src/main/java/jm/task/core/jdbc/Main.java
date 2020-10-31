4ad6302018656811d8043c6e7933033904d494d1
Ivan Pshychenko <68253439+Xarofive@users.noreply.github.com>
2020-10-31 11:23:36 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();
+//        userService.createUsersTable();
+//        userService.saveUser("32as33d", "12s22ad2", (byte) 12);
+//        userService.removeUserById(1);
+//        userService.getAllUsers();
+//        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/foo?serverTimezone=Europe/Moscow&useSSL=false",
+                    "root",
+                    "4815162342v");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
